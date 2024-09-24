478888bae1615b1936c4c075f962938470b91d61
kakoyo <kakoyo@qq.com>
2024-09-24 23:52:28 +0800

    添加项目文件。

---
+++ WpfForPrism/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+
+namespace WpfForPrism
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        /// <summary>
+        /// 返回项目的启动页。从容器中获取
+        /// </summary>
+        /// <returns></returns>
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        /// <summary>
+        /// 在这里注册服务到容器中。
+        /// </summary>
+        /// <param name="containerRegistry"></param>
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            
+        }
+    }
+
+}
